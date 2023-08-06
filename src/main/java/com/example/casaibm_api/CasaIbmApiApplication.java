7d2b0c6ca621c14b4b5c3f34b91377b2262f5877
Victor Vermaas <vhvermaas@gmail.com>
2023-08-06 15:42:23 -0300

    initialized project

---
+++ src/main/java/com/example/casaibm_api/CasaIbmApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.casaibm_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaIbmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaIbmApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/casaibm_api/CasaIbmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.casaibm_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaIbmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
