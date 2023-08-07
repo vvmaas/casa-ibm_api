8bacd72e576758b1b2617243cb49a79f9f8ffb4d
Victor Vermaas <vhvermaas@gmail.com>
2023-08-07 19:51:50 -0300

    fix: adicionei @CrossOrigin aos controllers

--- src/main/java/com/example/casaibm_api/controller/ReservaController.java
+++ src/main/java/com/example/casaibm_api/controller/ReservaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.example.casaibm_api.dto.ReservaDTO;
 import com.example.casaibm_api.service.ReservaService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/reservas")
 public class ReservaController {
 
